44294a5d8e682b1bac39cc2d288e94a1dcefce7b
Ken Ohata <ohata@daraf.co.jp>
2014-10-03 00:24:38 +0900

    define Bigticket::Rails::Engine

--- lib/bigticket.rb
+++ lib/bigticket.rb
@@ -1,2 +1,11 @@
+require 'bigticket/version'
+
 module Bigticket
+  module Rails
+    class Engine < ::Rails::Engine
+      initializer "", group: :all do |app|
+        app.config.assets.precompile += %w( bigticket/*.sass )
+      end
+    end
+  end
 end
